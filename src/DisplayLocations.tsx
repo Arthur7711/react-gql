1b5335b5fbb1c040f07dedd8f9711e0448c7bc3b
Arthur7711 <newartroll11@gmail.com>
2024-02-04 16:30:41 +0400

    feat: gql data types adding

--- src/DisplayLocations.tsx
+++ src/DisplayLocations.tsx
@@ -11,12 +11,21 @@ export function DisplayLocations() {
       }
     }
   `;
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
+  
+  interface IData {
+    locations: {
+      id: number;
+      name: string;
+      description: string;
+      photo: string;
+    }[];
+  }
+  const { loading, error, data } = useQuery<IData>(GET_LOCATIONS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  return data.locations.map(({ id, name, description, photo }) => (
+  return data?.locations.map(({ id, name, description, photo }) => (
     <div key={id}>
       <h3>{name}</h3>
       <img width="400" height="250" alt="location-reference" src={`${photo}`} />
