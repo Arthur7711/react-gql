b48d8c22ea38ddd92507e259b59aaa82152432c3
Arthur7711 <newartroll11@gmail.com>
2024-02-06 00:58:50 +0400

    feat: portal usage check

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { DisplayLocations } from "./DisplayLocations";
-
+import { createPortal } from "react-dom";
 
 function App() {
   return (
     <div>
       <h2>My first Apollo app 🚀</h2>
       <DisplayLocations />
+      {document.getElementById('modal') ? createPortal(
+        <p>This child is placed in the document body.</p>,
+        document.getElementById('modal')
+      ):''}
     </div>
   );
 }
